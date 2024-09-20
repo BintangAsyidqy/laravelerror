4ab1d7a1ccaa4fc55ed7a0496ada69746704a36f
BintangAsyidqy <muhammadbintangasyidqy@smkwikrama.sch.id>
2024-09-20 15:34:08 +0700

    first commit

---
+++ app/Http/Controllers/MedicineController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medicine;
+use Illuminate\Http\Request;
+
+class MedicineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('medicine.create');
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3',
+            'type' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        Medicine::create([
+            'name' => $request->$name,
+            'type' => $request->$type,
+            'price' => $request->$price,
+            'stock' => $request->$stock,
+        ]);
+
+        return redirect()->back()->with('succes', 'Berhasil Menambahkan Data Obat!');
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Medicine $medicine)
+    {
+        //
+    }
+}
